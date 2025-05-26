5121f4946fcf622e56ae16972ac4b179a5d1727d
Mikael Aditya N <mikaeladityan.99@gmail.com>
2025-05-27 02:05:28 +0700

    fix: bug button header

--- src/components/ui/package.tsx
+++ src/components/ui/package.tsx
@@ -5,7 +5,7 @@ import { IconCircleFilled } from "@tabler/icons-react";
 import { motion as m } from "motion/react";
 
 export const PackageButton = () => (
-	<m.button initial={{ opacity: 0, y: "-50%" }} animate={{ opacity: 1, y: 0 }} onClick={() => scrollToSection("#")} className="px-3 w-fit bg-amber-100 rounded-full py-1.5 text-sm font-bold flex items-center justify-start gap-2 text-gray-500 overflow-hidden">
+	<m.button type="button" initial={{ opacity: 0, y: "-50%" }} animate={{ opacity: 1, y: 0 }} onClick={(e) => scrollToSection(e, "#pricing")} className="px-3 w-fit bg-amber-100 rounded-full py-1.5 text-sm font-bold flex items-center justify-start gap-2 text-gray-500 overflow-hidden">
 		<div className="relative">
 			<IconCircleFilled size={14} className="text-amber-500" />
 			<IconCircleFilled size={14} className="text-amber-500 border border-amber-400 rounded-full animate-ping absolute top-0  z-[5]" />
