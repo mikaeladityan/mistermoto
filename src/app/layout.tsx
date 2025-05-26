0aad1672175e599ff969e33148eaa1c470500c11
Mikael Aditya N <mikaeladityan.99@gmail.com>
2025-05-27 02:12:59 +0700

    fix: meta header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { quicksand } from "~/utils/font";
 import { Provider } from "jotai";
@@ -11,8 +11,31 @@ export const metadata: Metadata = {
 		title: "Setrika Uap Gas Mistermoto – 25 Detik Rapi, Tanpa Listrik",
 		description: "Mesin setrika laundry profesional yang pangkas biaya hingga 30% dan percepat proses hingga 2× lipat. Order paket Anda sekarang!",
 	},
+	icons: {
+		icon: "/favicon.ico",
+		apple: "/apple-icon.png",
+		shortcut: "/favicon.ico",
+		other: [
+			{
+				rel: "mask-icon",
+				url: "/icon0.svg",
+				color: "#ffffff",
+			},
+		],
+	},
+	manifest: "/manifest.json",
+	authors: {
+		name: "Mikael Aditya Nugroho",
+		url: "https://instagram.com/mikaeladityan",
+	},
 };
 
+export const viewport: Viewport = {
+	themeColor: "white",
+	colorScheme: "normal",
+	width: "device-width",
+	initialScale: 1.0,
+};
 export default function RootLayout({
 	children,
 }: Readonly<{
